bd8e618dd929018e538f84925d045166de04452e
Phatdumpling <101686526+Phatdumpling@users.noreply.github.com>
2023-02-10 14:59:17 -0500

    Player Tile by tile movement

---
+++ CISC 226 Project/Assets/Scripts/Player_Controller_Tile_by_Tile.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.Tilemaps;
+
+public class Player_Controller_Tile_by_Tile : MonoBehaviour
+{
+    
+    [SerializeField]
+    private Tilemap groundTilemap;
+    [SerializeField]
+    private Tilemap collisionTileMap;
+    
+    
+    
+    private Player_Movement_Tile_by_Tile controls;
+    
+
+    private void Awake()
+    {
+        controls = new Player_Movement_Tile_by_Tile();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controls.Main.Movement.performed += ctx => Move(ctx.ReadValue<Vector2>());
+    }
+
+    private void Move(Vector2 direction)
+    {
+        if (CanMove(direction))
+        {
+            transform.position += (Vector3)direction;
+        }
+    }
+
+    private bool CanMove(Vector2 direction)
+    {
+        Vector3Int gridPosition = groundTilemap.WorldToCell(transform.position + (Vector3)direction);
+        if (!groundTilemap.HasTile(gridPosition) || collisionTileMap.HasTile(gridPosition))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+}
