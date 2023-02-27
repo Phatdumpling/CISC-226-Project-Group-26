28f20099554dafe7a36623b0d87b0b31afc4de54
Phatdumpling <101686526+Phatdumpling@users.noreply.github.com>
2023-02-26 21:04:11 -0500

    Future lv

---
+++ CISC 226 Project/Assets/Scripts/Future self move script/Future_Controller.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.Tilemaps;
+
+public class Future_Controller : MonoBehaviour
+{
+    
+    [SerializeField]
+    private Tilemap groundTilemap;
+    [SerializeField]
+    private Tilemap collisionTileMap;
+    [SerializeField] 
+    private Tilemap collision_Interactable;
+
+    private Vector2 previous;
+    
+    private Future_movement controls;
+
+    public LayerMask whatStopsMovement;
+    
+
+    private void Awake()
+    {
+        controls = new Future_movement();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controls.Main.Movement.performed += ctx => Move(ctx.ReadValue<Vector2>());
+    }
+
+    private void Move(Vector2 direction)
+    {
+        
+        if (CanMove(direction))
+        {
+            Vector3 prev = transform.position;
+            transform.position += (Vector3)direction;
+            
+            if (Physics2D.OverlapCircle(transform.position, 0.2f, whatStopsMovement))
+            {
+                transform.position = prev;
+            }
+            
+        }
+    }
+
+    private bool CanMove(Vector2 direction)
+    {
+        Vector3Int gridPosition = groundTilemap.WorldToCell(transform.position + (Vector3)direction);
+        
+        if (!groundTilemap.HasTile(gridPosition) || collisionTileMap.HasTile(gridPosition)||collision_Interactable.HasTile(gridPosition))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
