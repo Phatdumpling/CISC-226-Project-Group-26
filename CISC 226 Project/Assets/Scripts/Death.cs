88692b3074f6b75036461d5201c00f08383c29b5
Phatdumpling <101686526+Phatdumpling@users.noreply.github.com>
2023-03-04 13:05:47 -0500

    Added start and game over screen

---
+++ CISC 226 Project/Assets/Scripts/Death.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public LayerMask whatdeath;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics2D.OverlapCircle(transform.position, 0.2f, whatdeath))
+        {
+            SceneManager.LoadScene("End Screen");
+        }
+    }
+}
---
+++ CISC 226 Project/Assets/Scripts/follow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class follow : MonoBehaviour
+{
+    public Transform followthis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = followthis.position;
+    }
+}
