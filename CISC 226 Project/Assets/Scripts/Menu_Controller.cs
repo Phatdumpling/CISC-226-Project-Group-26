4fb3119718f22340148be93255c64ee9f4dc288d
Phatdumpling <101686526+Phatdumpling@users.noreply.github.com>
2023-03-18 15:53:46 -0400

    How to play

--- CISC 226 Project/Assets/Scripts/Menu_Controller.cs
+++ CISC 226 Project/Assets/Scripts/Menu_Controller.cs
@@ -4,11 +4,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartMenu : MonoBehaviour
+public class Menu_Controller : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void StartGame()
+    public void LoadLevel1()
     {
         SceneManager.LoadScene("Past self saving");
+    }
+
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene("How to play");
+    }
+
+    public void StartMenu()
+    {
+        SceneManager.LoadScene("Start Screen");
     }
 }
