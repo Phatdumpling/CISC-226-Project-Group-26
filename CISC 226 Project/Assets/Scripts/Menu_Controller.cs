323ee3c2d2ede43a4c990febee2b0ff06fa02bc5
Phatdumpling <101686526+Phatdumpling@users.noreply.github.com>
2023-03-18 23:19:44 -0400

    options menu

--- CISC 226 Project/Assets/Scripts/Menu_Controller.cs
+++ CISC 226 Project/Assets/Scripts/Menu_Controller.cs
@@ -6,6 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class Menu_Controller : MonoBehaviour
 {
+
+    [SerializeField] private GameObject thisObj;
+    [SerializeField] private GameObject IsParent;
+    
+    
     // Start is called before the first frame update
     public void LoadLevel1()
     {
@@ -21,4 +26,26 @@ public class Menu_Controller : MonoBehaviour
     {
         SceneManager.LoadScene("Start Screen");
     }
+
+    public void OptionsMenu()
+    {
+        SceneManager.LoadScene("Options");
+    }
+    
+    
+    /* DO NOT UNCOMMENT
+    public void HowToPlayPopUp()
+    {
+        Instantiate(thisObj);
+        thisObj.transform.parent = IsParent.transform;
+        thisObj.transform.localScale =- new Vector3(-0.5f, 0.5f, 0.5f);
+    }
+    
+    public void HowToPlayPopDown()
+    {
+        
+    }
+    */
+    
+    
 }
