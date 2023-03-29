84e9d0092cfada3ec1f84c934d07f29b24424bad
Phatdumpling <101686526+Phatdumpling@users.noreply.github.com>
2023-03-29 12:35:20 -0400

    Volume save

--- CISC 226 Project/Assets/Scripts/Option_menu.cs
+++ CISC 226 Project/Assets/Scripts/Option_menu.cs
@@ -1,16 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UIElements;
+using Slider = UnityEngine.UI.Slider;
 
 public class Option_menu : MonoBehaviour
 {
 
     public AudioMixer audioMixer;
     
+    public Slider volumeSlider;
+    
     
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("Volume", volume);
     }
+
+    
+    void Start()
+    {
+        audioMixer.GetFloat("Volume", out float volume);
+        volumeSlider.value = volume;
+        Debug.Log(volumeSlider.value);
+        Debug.Log(volume);
+    }
+    
 }
---
+++ CISC 226 Project/Assets/Scripts/volumeSlider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class volumeSlider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioMixer audioMixer;
+    public Slider theslider;
+    public void Start()
+    {
+        audioMixer.GetFloat("Master", out float volume);
+        //this.value = volume;
+    }
+}
