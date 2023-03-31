c829697d61074a7c58a4fcdb8c9ffbfe9e0e0a7f
Phatdumpling <101686526+Phatdumpling@users.noreply.github.com>
2023-03-31 11:31:53 -0400

    Audio done

---
+++ CISC 226 Project/Assets/Scripts/PauseAudio.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UIElements;
+using Slider = UnityEngine.UI.Slider;
+
+public class PauseAudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioMixer audioMixer;
+    
+    public Slider MasterSlider;
+    
+    public Slider MusicSlider;
+    
+    public Slider SFXSlider;
+    void Start()
+    {
+        audioMixer.GetFloat("Volume", out float MasterVolume);
+        MasterSlider.value = MasterVolume;
+        
+        audioMixer.GetFloat("Music", out float MusicVolume);
+        MusicSlider.value = MusicVolume;
+        
+        audioMixer.GetFloat("SFX", out float SFXrVolume);
+        SFXSlider.value = SFXrVolume;
+    }
+    
+    public void SetMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+    
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicExpose", volume);
+    }
+    
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXExpose", volume);
+    }
+    
+}
