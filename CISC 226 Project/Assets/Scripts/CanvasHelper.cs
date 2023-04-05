b23262f8932c2e6a90ed399326cd25511012cf7b
Phatdumpling <101686526+Phatdumpling@users.noreply.github.com>
2023-04-05 00:39:15 -0400

    ooga

---
+++ CISC 226 Project/Assets/Scripts/CanvasHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasHelper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Canvas theCanvas = GetComponentInParent<Canvas>();
+        theCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+    }
+}
